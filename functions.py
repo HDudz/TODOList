33150b2c355548eb926dc7813f5de6a130718af3
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2024-10-26 18:49:30 +0200

    Moved file operations functions here, added delete_lists() function.

---
+++ functions.py
@@ -0,0 +1,19 @@
+import json
+
+
+def load_todo_lists(file_path):
+    """Loads lists from JSON."""
+    with open(file_path, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+def save_todo_lists(lists, file_path):
+    """Saves lists to JSON."""
+    json_object = json.dumps(lists, indent=4)
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json_file.write(json_object)
+
+def delete_lists(lists, title, file_path):
+    selected_list = next((todo for todo in lists if todo["title"] == title), None)
+    if selected_list:
+        lists.remove(selected_list)
+        save_todo_lists(lists, file_path)
