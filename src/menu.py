d363f38cbb111474a6c72aae7981332a377964b0
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2024-10-28 20:43:03 +0100

    Added working quit button on the bottom of the menu.

--- src/menu.py
+++ src/menu.py
@@ -1,18 +1,30 @@
 import ttkbootstrap as ttk
+from functions import *
 
 
 class Menu:
-    def __init__(self, root, main_page):
-        menuFrame = ttk.Frame(root)
-        menuFrame.grid(column=0, row=0, sticky="nsew")
+    def __init__(self, app, main_page):
+        self.root = app.root
+        self.main_page = main_page
+        self.main_frame = self.main_page.mainPageFrame
+        self.menuFrame = ttk.Frame(self.root)
+        self.menuFrame.grid(column=0, row=0, sticky="nsew")
 
-        menuFrame.columnconfigure(0, weight=1)
-        menuFrame.rowconfigure((0, 1), weight=0)
+        self.buttonPadx = (5,5)
 
-        titleLabel = ttk.Label(menuFrame, text="  TODO\nManager", font=("roboto", 20, "bold", "italic"))
-        choiceButton = ttk.Button(menuFrame, text="Choose list", style="outline-light", command=main_page.switch_to_choice)
-        settingsButton = ttk.Button(menuFrame, text="Settings", style="outline-light")
+        self.menuFrame.columnconfigure(0, weight=1)
+        self.menuFrame.rowconfigure((0, 1, 2), weight=0)
+        self.menuFrame.rowconfigure(3, weight=1)
 
-        titleLabel.grid(column=0, row=0, sticky="n", pady=(20, 10), padx=(35,35))
-        choiceButton.grid(column=0, row=1, pady=(10, 5), sticky="ew")
-        settingsButton.grid(column=0, row=2, pady=(5, 10), sticky="ew")
+        self.titleLabel = ttk.Label(self.menuFrame, text="  TODO\nManager", font=("roboto", 20, "bold", "italic"))
+        self.choiceButton = ttk.Button(self.menuFrame, text="Choose list", style="outline-light", command=lambda frame=self.main_frame: switch_to(frame))
+        self.settingsButton = ttk.Button(self.menuFrame, text="Settings", style="outline-light")
+        self.quitButton = ttk.Button(self.menuFrame, text="Quit", style="outline-danger", command=self.root.destroy)
+
+        self.titleLabel.grid(column=0, row=0, sticky="n", pady=(20, 10), padx=(35,35))
+        self.choiceButton.grid(column=0, row=1, pady=(10, 5), padx=self.buttonPadx, sticky="ew")
+        self.settingsButton.grid(column=0, row=2, pady=(5, 5), padx=self.buttonPadx, sticky="ew")
+        self.quitButton.grid(column=0, row=3, pady=(5, 40), padx=self.buttonPadx, sticky="sew")
+
+
+
