3082e8cf6083daec5a6566d87369306b19e918a6
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2024-11-17 18:33:42 +0100

    Editing now works, need to fix adding new taska.

--- src/editPage.py
+++ src/editPage.py
@@ -1,13 +1,17 @@
+from tkinter.constants import *
 import ttkbootstrap as ttk
 from ttkbootstrap.scrolled import ScrolledFrame
 from functions import *
+from icons import icons
 
 
 class EditPage:
-    def __init__(self, app, title):
+    def __init__(self, app, main_page, title):
         self.app = app
         self.root = app.root
         self.title = title
+        self.main_page = main_page
+
 
         self.file_path = '../data/lists.json'
         self.frame = ttk.Frame(self.root, style='secondary')
@@ -15,6 +19,8 @@ class EditPage:
         self.frame.lower()
 
         self.lists = load_todo_lists(self.file_path)
+
+        self.selected_list = next((todo for todo in self.lists if todo["title"] == self.title), None)
 
         self.is_reset = True
         self.tasks = []
@@ -30,32 +36,58 @@ class EditPage:
         for widget in self.frame.winfo_children():
             widget.destroy()
 
-
+        self.tasks = []
 
         editLabel = ttk.Label(self.frame, text="Editing list:", font=('roboto', 20, 'bold'), background=self.app.sec_color)
-        nameEntry = ttk.Entry(self.frame, font=('roboto', 15))
-        nameEntry.insert(0, self.title)
+        self.nameEntry = ttk.Entry(self.frame, font=('roboto', 15))
+        self.nameEntry.insert(0, self.title)
 
         editLabel.grid(column=0, row=0, sticky='n', pady=(20, 0), padx=(20, 0))
-        nameEntry.grid(column=0, row=1, sticky='new', pady=(0, 30), padx=(100, 100))
+        self.nameEntry.grid(column=0, row=1, sticky='new', pady=(0, 30), padx=(100, 100))
 
         editFrame = ScrolledFrame(self.frame, style='secondary.TFrame', autohide=True)
         editFrame.grid(column=0, row=2, sticky="nwes")
         editFrame.container.configure(style="secondary.TFrame")
         editFrame.container.columnconfigure(0, weight=1)
+        editFrame.container.columnconfigure(1, weight=0)
 
 
-        selected_list = next((todo for todo in self.lists if todo["title"] == self.title), None)
 
-        for i, task in enumerate(selected_list["todo"], start=1):
+
+        for i, task in enumerate(self.selected_list["todo"], start=1):
             task_edit = ttk.Entry(editFrame.container, font=('roboto', 12))
             task_edit.insert(0, task)
-            task_edit.grid(column=0, row=i, sticky='ew', padx=(20, 20), pady=(0, 10))
+            delete_button = ttk.Button(editFrame.container, style="danger", width=4, image=icons.whiteDel, compound=CENTER, command=lambda index=i - 1: self.delete_task(index) )
+
+
+            task_edit.grid(column=0, row=i, sticky='ew', padx=(20, 0), pady=(10, 0))
+            delete_button.grid(column=1, row=i, sticky='ew', padx=(0, 20), pady=(10, 0))
+
             self.tasks.append(task_edit)
 
-        submitButton = ttk.Button(self.frame, text="Submit", style="success", width=10)
+        add_new_list_button = ttk.Button(editFrame.container, text="Add new task", style="success", command=self.add_new_task)
+        add_new_list_button.grid(column=0, row=len(self.selected_list["todo"]) + 1, columnspan=1, sticky='ew', pady=(10, 0), padx=(20, 20))
+
+
+        submitButton = ttk.Button(self.frame, text="Submit", style="success", width=10, command=self.submit)
         submitButton.grid(column=0, row=3, pady=(5, 40), sticky="s")
 
     def show(self):
         self.setup()
         self.frame.tkraise()
+
+    def submit(self):
+        self.selected_list["title"] = self.nameEntry.get()
+        for i, task in enumerate(self.tasks, start=0):
+            self.selected_list["todo"][i] = task.get()
+        save_todo_lists(self.lists, self.file_path)
+        self.main_page.show()
+
+    def delete_task(self, index):
+        del self.selected_list["todo"][index]
+        self.setup()
+
+    def add_new_task(self):
+        self.selected_list["todo"].append("")
+        self.setup()
+
